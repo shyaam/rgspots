86dcc29f294db24e216b7f7637bcffe055830431
Shyaam Deshmukh <shyaam.deshmukh@gmail.com>
2014-07-29 20:14:15 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rgspots::Application.config.secret_key_base = '8e648a12bd3182b1c91fcc012191620fe9206d87357e3a3d27a3c03a30febad0781ed711b0cd604188141cb32dcfdbd4528fe08f1bba248315e8339a8bb8cc67'
